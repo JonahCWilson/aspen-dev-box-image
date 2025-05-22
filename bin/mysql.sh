44194fdb32454de02e29d951f82f1257ad2f7893
kenstir <kenstir@gmail.com>
2025-05-22 12:46:12 -0400

    I was making this too hard

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-MARIADB_DATABASE=aspen
-MARIADB_USER=aspensuper
-MARIADB_PASSWORD=aspensuper
-
-docker run -it --network aspen-net \
-  --rm mariadb \
-  mariadb \
-  --host=aspen-db \
-  --database=$MARIADB_DATABASE \
-  --user=$MARIADB_USER \
-  --password=$MARIADB_PASSWORD \
-  "$@"
+docker exec -it aspen-db mariadb -u root -paspen aspen "$@"
