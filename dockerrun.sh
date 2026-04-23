0e3932f2fc03b690723592e50779050aed4db7ae
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-04-23 13:58:08 +0100

    Chown CONFIG_DIRECTORY so www-data can write site config

--- dockerrun.sh
+++ dockerrun.sh
@@ -53,6 +53,7 @@ mkdir -p /data/aspen-discovery/${SITENAME}/{covers/{small,large,medium,original}
 mkdir -p /var/log/aspen-discovery/${SITENAME}/logs
 mkdir -p /var/run/aspen
 chown -R www-data:www-data /usr/local/aspen-discovery/tmp /var/log/aspen-discovery /data/aspen-discovery/${SITENAME}
+chown -R www-data:www-data "${CONFIG_DIRECTORY}"
 chown -R aspen:www-data /var/run/aspen
 cp "${CONFIG_DIRECTORY}/httpd-${SITENAME}.conf" /etc/apache2/sites-enabled/
 cp "${CONFIG_DIRECTORY}/conf/php-fpm.conf" /etc/php/8.4/fpm/pool.d/
